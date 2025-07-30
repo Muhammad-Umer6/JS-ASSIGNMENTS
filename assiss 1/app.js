ee224b44518299c6950a1f5772c31a73bc262142
Muhammad-Umer6 <umerzaheer.pro@gmail.com>
2025-07-31 00:28:30 +0500

    added

--- assiss 1/app.js
+++ assiss 1/app.js
@@ -40,7 +40,7 @@
         
     //     for (let i = 0; i < str.length; i++) {
     //       reversed = str[i] + reversed;
-    //     }
+    //     } 
         
     //     return reversed;
     //   }
